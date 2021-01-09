9be63d4c770db196c6d3cf7652f836500bfb18fb
Dijango Alves <dijango_ilha@hotmail.com>
2021-01-09 14:19:07 -0300

    feat: add Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
+
+import Home from "./pages/home";
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Home() {
+  return (
+    <>
+      <header>Header</header>
+      <h1>Find place</h1>
+      <h1>Cards</h1>
+    </>
+  );
+}
+
+export default Home;
