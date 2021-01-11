92335b1b75502ce90c519c6d9a9c260919024818
Dijango Alves <dijango_ilha@hotmail.com>
2021-01-11 10:22:43 -0300

    feat: add header

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+import MyLogo from "../../img/Lugares 1.png";
+
+const Logo = styled.div`
+  background-image: url(${MyLogo});
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100%;
+  margin-left: 53px;
+`;
+
+const Header = styled.header`
+  background: #000;
+  height: 85px;
+`;
 
 export function Home() {
   return (
     <>
-      <header>Header</header>
+      <Header>
+        <Logo></Logo>
+      </Header>
       <h1>Find place</h1>
       <h1>Cards</h1>
     </>
